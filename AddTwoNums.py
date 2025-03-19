d77ed82d4f30faacb79e029838e69474b5ede3d4
nozerging <1976729868@qq.com>
2025-03-19 09:28:45 +0800

    4

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,10 +1,18 @@
 #Add Two Numbers in Pyhton
 #Author:cxy
-#Using user input
+#Using a function
+
+#function to add two numbers
+def add(a,b):
+ #converting input to floatand adding
+ result = float(a) + float(b)
+ return result
 
 #taking user input
 a = input("First number: ")
 b = input("Second number: ")
-#converting input to float and adding
-res = float(a) + float(b)
+
+#calling function
+res = add(a,b)
+print("The Answer is: ")
 print(res)
