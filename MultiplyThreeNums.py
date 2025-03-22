b8ef7eee3bfca1512dac89c379846034f77a3a81
zest <1976729868@qq.com>
2025-03-22 15:12:23 +0000

    mu

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("enter the second number: "))
+num3 = float(input("enter the third number: "))
+product = num1*num2*num3
+print(f"the product is:{product}")
